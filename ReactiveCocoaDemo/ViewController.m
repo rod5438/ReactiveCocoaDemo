eeee6f569877904642b22c0f84fdfac24115c416
吳鴻 2 <jason@jasondembp.pft.com>
2016-03-23 14:37:45 +0800

    [What] Implement Slider bar and label

--- ReactiveCocoaDemo/ViewController.m
+++ ReactiveCocoaDemo/ViewController.m
@@ -10,6 +10,13 @@
 
 @interface ViewController ()
 
+
+@property IBOutlet UISlider *MVC_Slider;
+@property IBOutlet UILabel *MVC_Label;
+
+@property IBOutlet UISlider *MVVM_Slider;
+@property IBOutlet UILabel *MVVM_Label;
+
 @end
 
 @implementation ViewController
@@ -22,6 +29,12 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)valueChanged:(UISlider *)sender
+{
+    NSString *displayNumber = [NSString stringWithFormat:@"%.4lf", sender.value];
+    self.MVC_Label.text = displayNumber;
 }
 
 @end
