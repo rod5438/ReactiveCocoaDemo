175dcdbc1ddb564bd8fb6c3ec44068a18247fa5e
吳鴻 2 <jason@jasondembp.pft.com>
2016-03-24 12:37:08 +0800

    [What] Implement BMI with reactiveCocoa

--- ReactiveCocoaDemo/ViewController.m
+++ ReactiveCocoaDemo/ViewController.m
@@ -5,8 +5,9 @@
 //  Created by Jason Wu on 2016/3/23.
 //  Copyright © 2016年 Jason Wu. All rights reserved.
 //
-
+// Update to revision
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController () <UITextFieldDelegate>
 
@@ -26,62 +27,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldTextDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
+    
+    RAC(self, height) = [self.heightTextField.rac_textSignal map:^id(NSString *text) {
+        return [NSNumber numberWithFloat:[text floatValue]];
+    }];
+    RAC(self, weight) = [self.weightTextField.rac_textSignal map:^id(NSString *text) {
+        return [NSNumber numberWithFloat:[text floatValue]];
+    }];
+    RAC(self, BMI) = [RACSignal combineLatest:@[RACObserve(self, height), RACObserve(self, weight)] reduce:^id (NSNumber *height, NSNumber *weight) {
+        return @([weight floatValue] / powf([height floatValue] / 100, 2));
+    }];
+    RAC(self.BMITextField, text) = [RACObserve(self, BMI) map:^id(NSNumber *BMI) {
+        return [NSString stringWithFormat:@"%.2lf",[BMI floatValue]];
+    }];
+    RAC(self.resultTextField, text) = [RACObserve(self, BMI) map:^id(NSNumber *BMInumber) {
+        NSString *result;
+        CGFloat BMI = [BMInumber floatValue];
+        if (BMI < 18.5) {
+            result = @"體重過輕";
+        }
+        else if (BMI >=18.5 && BMI < 24.0) {
+            result = @"健康體位";
+        }
+        else if (BMI >=24.0 && BMI < 27.0) {
+            result = @"快要過重";
+        }
+        else if (BMI >=27.0 && BMI < 30.0) {
+            result = @"輕度肥胖";
+        }
+        else if (BMI >=30.0 && BMI < 35.0) {
+            result = @"中度肥胖";
+        }
+        else if (BMI >= 35.0) {
+            result = @"重度肥胖";
+        }
+        else {
+            result = nil;
+        }
+        return result;
+    }];
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)textFieldTextDidChange:(id)sender
-{
-    [self updateBMI];
-    [self updateTextField];
-}
-
-- (void)updateBMI
-{
-    self.height = [self.heightTextField.text floatValue];
-    self.weight = [self.weightTextField.text floatValue];
-    self.BMI = self.weight / (self.height/100 * self.height/100);
-}
-
-- (void)updateTextField
-{
-    [self updateBMITextField];
-    [self updateResultTextField];
-}
-
-- (void)updateBMITextField
-{
-    self.BMITextField.text = [NSString stringWithFormat:@"%.2lf", self.BMI];
-}
-
-- (void)updateResultTextField
-{
-    if (self.BMI < 18.5) {
-        self.resultTextField.text = @"體重過輕";
-    }
-    else if (self.BMI >=18.5 && self.BMI < 24.0) {
-        self.resultTextField.text = @"健康體位";
-    }
-    else if (self.BMI >=24.0 && self.BMI < 27.0) {
-        self.resultTextField.text = @"快要過重";
-    }
-    else if (self.BMI >=27.0 && self.BMI < 30.0) {
-        self.resultTextField.text = @"輕度肥胖";
-    }
-    else if (self.BMI >=30.0 && self.BMI < 35.0) {
-        self.resultTextField.text = @"中度肥胖";
-    }
-    else if (self.BMI >= 35.0) {
-        self.resultTextField.text = @"重度肥胖";
-    }
-    else {
-        self.resultTextField.text = nil;
-    }
 }
 
 @end
